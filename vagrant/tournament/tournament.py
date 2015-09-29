8ca0c935e50247fb8ea36a187f1719d141753067
Gustavo Molina <gu_molina@hotmail.com>
2015-09-28 22:41:31 -0300

    Updated tournament files to run the test

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,15 +13,28 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches")
+    conn.commit()
+    conn.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players")
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("select count(*) from players")
+    result = c.fetchone()
+    conn.close()
+    return result[0]
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -32,7 +45,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO players(name) VALUES (%s);",[name])
+    conn.commit()
+    conn.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +64,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("select player_id, player_name, wins, wins+losses as matches from rank")
+    result = c.fetchall()
+    conn.close()
+    return result
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,7 +78,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches(winner_id,loser_id) VALUES (%s,%s);",[winner,loser])
+    conn.commit()
+    conn.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -73,5 +99,21 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("select player_id, player_name from rank")
+    rows = c.fetchall()
+    conn.close()
 
+    result = []
 
+    while rows:
+        aux = 1
+        player1 = rows[0]
+        player2 = rows[aux]
+        del rows[aux]
+        del rows[0]
+        result.append(player1 + player2)
+        
+        
+    return result
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,25 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP database tournament;
+create database tournament;
+\c tournament
 
+CREATE TABLE players (
+   ID  SERIAL PRIMARY KEY,
+   name VARCHAR(100) NOT NULL
+   );
+
+CREATE TABLE matches (
+   ID  SERIAL PRIMARY KEY,
+   winner_id int,
+   loser_id int
+   );
+
+CREATE VIEW rank AS 
+	SELECT  p.id as player_id, p.name player_name, count(w.id) as wins, count(l.id) as losses 
+				from players p 
+            left join matches w on w.winner_id = p.id
+            left join matches l on l.loser_id = p.id
+            group by p.id
+            order by wins desc;
